1c5cb892f3fbf02282c9f9f8be88240c7174ad8a
devfast01 <iot123tech@gmail.com>
2023-05-27 18:28:22 +0500

    Recycler View

---
+++ app/src/main/java/com/example/kotlin/ItemsViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin
+
+data class ItemsViewModel(val image: Int, val text: String) {
+}
--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -1,76 +1,35 @@
 package com.example.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-
-
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    // An int variable to hold a value
-    private var value = 0
-
-
-    override fun onClick(v: View) {
-        // A local variable to use later
-        val size: Float
-
-        when (v.id) {
-            R.id.btnAdd -> {
-                value++
-                txtValue.text = "$value"
-            }
-
-            R.id.btnTake -> {
-                value--
-                txtValue.text = "$value"
-            }
-
-            R.id.btnReset -> {
-                value = 0
-                txtValue.text = "$value"
-            }
-
-            R.id.btnGrow -> {
-                size = txtValue.textScaleX
-                txtValue.textScaleX = size + 1
-            }
-
-            R.id.btnShrink -> {
-                size = txtValue.textScaleX
-                txtValue.textScaleX = size - 1
-            }
-
-            R.id.btnHide -> if (txtValue.visibility == View.VISIBLE) {
-                // Currently visible so hide it
-                txtValue.visibility = View.INVISIBLE
-
-                // Change text on the button
-                btnHide.text = "SHOW"
-
-            } else {
-                // Currently hidden so show it
-                txtValue.visibility = View.VISIBLE
-
-                // Change text on the button
-                btnHide.text = "HIDE"
-            }
-        }
-    }
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Listen for all the button clicks
-        btnAdd.setOnClickListener(this)
-        btnTake.setOnClickListener(this)
-        txtValue.setOnClickListener(this)
-        btnGrow.setOnClickListener(this)
-        btnShrink.setOnClickListener(this)
-        btnReset.setOnClickListener(this)
-        btnHide.setOnClickListener(this)
+        // getting the recyclerview by its id
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ItemsViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(ItemsViewModel(R.drawable.ic_launcher_background, "Item " + i))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CustomAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
 
     }
 }
